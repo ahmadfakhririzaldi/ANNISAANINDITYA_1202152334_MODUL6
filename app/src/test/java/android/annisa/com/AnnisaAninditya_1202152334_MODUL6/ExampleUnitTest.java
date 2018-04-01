4470a3f0b926ff6e8a3f323e7f2ec6716c873ffb
pradiptaramadhan <pradipta.ramadhan22@gmail.com>
2018-04-01 18:51:16 +0700

    first commit

---
+++ app/src/test/java/android/annisa/com/AnnisaAninditya_1202152334_MODUL6/ExampleUnitTest.java
@@ -0,0 +1,13 @@
+package android.annisa.com.AnnisaAninditya_1202152334_MODUL6;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
